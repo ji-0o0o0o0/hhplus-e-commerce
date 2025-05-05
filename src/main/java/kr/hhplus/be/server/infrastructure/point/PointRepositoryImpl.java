6a028eabc753e7c09544d0c0e3752f0d2a40f0c8
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-05-05 13:45:38 +0900

    feat Point Infrastructure Layer 작성 #14

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/point/PointRepositoryImpl.java
@@ -0,0 +1,37 @@
+package kr.hhplus.be.server.infrastructure.point;
+
+import kr.hhplus.be.server.domain.point.Point;
+import kr.hhplus.be.server.domain.point.PointHistory;
+import kr.hhplus.be.server.domain.point.PointRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class PointRepositoryImpl implements PointRepository {
+    private final PointJpaRepository pointJpaRepository;
+    private PointHistoryJpaRepository pointHistoryJpaRepository;
+    @Override
+    public Optional<Point> findPointByUserId(Long userId) {
+        return pointJpaRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<PointHistory> findPointHistoryByUserId(Long userId) {
+        return pointHistoryJpaRepository.findAllByPoint_UserId(userId);
+    }
+
+    @Override
+    public void savePoint(Point point) {
+        pointJpaRepository.save(point);
+    }
+
+
+    @Override
+    public void savePointHistory(PointHistory pointHistory) {
+        pointHistoryJpaRepository.save(pointHistory);
+    }
+}
